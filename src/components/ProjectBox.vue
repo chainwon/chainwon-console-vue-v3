3a2ccdc4b04e584defd8a0aa2502a52882b0c67b
折影轻梦 <776194970@qq.com>
2019-04-21 12:25:34 +0800

    修复按钮显示bug

--- src/components/ProjectBox.vue
+++ src/components/ProjectBox.vue
@@ -24,7 +24,8 @@ export default {
   data() {
     return {
       gutter: 20,
-      screenWidth: document.body.clientWidth
+      screenWidth: document.body.clientWidth,
+      project: []
     };
   },
   created() {
