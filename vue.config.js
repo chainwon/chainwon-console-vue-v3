4468b7ee7a3bf89862701c9cba3ec38926a3abd1
折影轻梦 <nexmoe@qq.com>
2021-07-14 11:40:13 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: "https://console.chainwon.com/api/",
+                target: "https://console.chainwon.com/api/index.php/",
                 ws: true,
                 changeOrigin: true,
                 pathRewrite:{
